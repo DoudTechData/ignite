ca31faa0ea42f3bab33a3264ab408b8666bc5762
sboikov <sboikov@gridgain.com>
2016-01-13 15:04:16 +0300

    ignite-1811 Optimized cache 'get' on affinity node.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/IgniteCacheGetRestartTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/IgniteCacheGetRestartTest.java
@@ -17,19 +17,31 @@
 
 package org.apache.ignite.internal.processors.cache.distributed;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteDataStreamer;
 import org.apache.ignite.cache.CacheMode;
-import org.apache.ignite.cache.CacheRebalanceMode;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.configuration.NearCacheConfiguration;
+import org.apache.ignite.internal.IgniteInternalFuture;
+import org.apache.ignite.internal.processors.cache.IgniteCacheProxy;
+import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.TcpDiscoveryIpFinder;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
+import org.apache.ignite.testframework.GridTestUtils;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 
-import static org.apache.ignite.cache.CacheRebalanceMode.*;
+import static org.apache.ignite.cache.CacheMode.PARTITIONED;
+import static org.apache.ignite.cache.CacheMode.REPLICATED;
+import static org.apache.ignite.cache.CacheRebalanceMode.ASYNC;
 
 /**
  *
@@ -39,7 +51,7 @@ public class IgniteCacheGetRestartTest extends GridCommonAbstractTest {
     private static final TcpDiscoveryIpFinder ipFinder = new TcpDiscoveryVmIpFinder(true);
 
     /** */
-    private static final long TEST_TIME = 10_000;
+    private static final long TEST_TIME = 60_000;
 
     /** */
     private static final int SRVS = 3;
@@ -85,25 +97,157 @@ public class IgniteCacheGetRestartTest extends GridCommonAbstractTest {
         }
     }
 
+    /** {@inheritDoc} */
+    @Override protected void afterTestsStopped() throws Exception {
+        super.afterTestsStopped();
+
+        stopAllGrids();
+    }
+
+    /** {@inheritDoc} */
+    @Override protected long getTestTimeout() {
+        return TEST_TIME + 60_000;
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testGetRestartReplicated() throws Exception {
+        CacheConfiguration<Object, Object> cache = cacheConfiguration(REPLICATED, 0, false);
+
+        checkRestart(cache, 3);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testGetRestartPartitioned1() throws Exception {
+        CacheConfiguration<Object, Object> cache = cacheConfiguration(PARTITIONED, 1, false);
+
+        checkRestart(cache, 1);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testGetRestartPartitioned2() throws Exception {
+        CacheConfiguration<Object, Object> cache = cacheConfiguration(PARTITIONED, 2, false);
+
+        checkRestart(cache, 2);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testGetRestartPartitionedNearEnabled() throws Exception {
+        CacheConfiguration<Object, Object> cache = cacheConfiguration(PARTITIONED, 1, true);
+
+        checkRestart(cache, 1);
+    }
+
     /**
      * @param ccfg Cache configuration.
      * @param restartCnt Number of nodes to restart.
      * @throws Exception If failed.
      */
-    private void checkRestart(CacheConfiguration ccfg, int restartCnt) throws Exception {
+    private void checkRestart(final CacheConfiguration ccfg, final int restartCnt) throws Exception {
         ignite(0).createCache(ccfg);
 
-        if (ccfg.getNearConfiguration() != null)
-            ignite(SRVS).createNearCache(ccfg.getName(), new NearCacheConfiguration<>());
+        try {
+            if (ccfg.getNearConfiguration() != null)
+                ignite(SRVS).createNearCache(ccfg.getName(), new NearCacheConfiguration<>());
 
-        try (IgniteDataStreamer<Object, Object> streamer = ignite(0).dataStreamer(ccfg.getName())) {
-            for (int i = 0; i < KEYS; i++)
-                streamer.addData(i, i);
+            try (IgniteDataStreamer<Object, Object> streamer = ignite(0).dataStreamer(ccfg.getName())) {
+                for (int i = 0; i < KEYS; i++)
+                    streamer.addData(i, i);
+            }
+
+            final long stopTime = U.currentTimeMillis() + TEST_TIME;
+
+            final AtomicInteger nodeIdx = new AtomicInteger();
+
+            IgniteInternalFuture<?> fut1 = GridTestUtils.runMultiThreadedAsync(new Callable<Void>() {
+                @Override public Void call() throws Exception {
+                    IgniteCache<Object, Object> cache = ignite(nodeIdx.getAndIncrement()).cache(ccfg.getName());
+
+                    while (U.currentTimeMillis() < stopTime)
+                        checkGet(cache);
+
+                    return null;
+                }
+            }, SRVS + CLIENTS, "get-thread");
+
+            final AtomicInteger restartNodeIdx = new AtomicInteger(SRVS + CLIENTS);
+
+            final AtomicBoolean clientNode = new AtomicBoolean();
+
+            IgniteInternalFuture<?> fut2 = GridTestUtils.runMultiThreadedAsync(new Callable<Void>() {
+                @Override public Void call() throws Exception {
+                    int nodeIdx = restartNodeIdx.getAndIncrement();
+
+                    boolean clientMode = clientNode.compareAndSet(false, true);
+
+                    while (U.currentTimeMillis() < stopTime) {
+                        if (clientMode)
+                            client.set(true);
+
+                        log.info("Restart node [node=" + nodeIdx + ", client=" + clientMode + ']');
+
+                        Ignite ignite = startGrid(nodeIdx);
+
+                        IgniteCache<Object, Object> cache;
+
+                        if (clientMode && ccfg.getNearConfiguration() != null)
+                            cache = ignite.createNearCache(ccfg.getName(), new NearCacheConfiguration<>());
+                        else
+                            cache = ignite.cache(ccfg.getName());
+
+                        checkGet(cache);
+
+                        IgniteInternalFuture<?> syncFut = ((IgniteCacheProxy)cache).context().preloader().syncFuture();
+
+                        while (!syncFut.isDone())
+                            checkGet(cache);
+
+                        checkGet(cache);
+
+                        stopGrid(nodeIdx);
+                    }
+
+                    return null;
+                }
+            }, restartCnt + 1, "restart-thread");
+
+            fut1.get();
+            fut2.get();
         }
-
-        long stopTime = System.currentTimeMillis() + TEST_TIME;
+        finally {
+            ignite(0).destroyCache(ccfg.getName());
+        }
     }
 
+    /**
+     * @param cache Cache.
+     */
+    private void checkGet(IgniteCache<Object, Object> cache) {
+        for (int i = 0; i < KEYS; i++)
+            assertEquals(i, cache.get(i));
+
+        Set<Integer> keys = new HashSet<>();
+
+        for (int i = 0; i < KEYS; i++) {
+            keys.add(i);
+
+            if (keys.size() == 100) {
+                Map<Object, Object> vals = cache.getAll(keys);
+
+                for (Object key : keys)
+                    assertEquals(key, vals.get(key));
+
+                keys.clear();
+            }
+        }
+    }
 
     /**
      * @param cacheMode Cache mode.
@@ -116,13 +260,13 @@ public class IgniteCacheGetRestartTest extends GridCommonAbstractTest {
 
         ccfg.setCacheMode(cacheMode);
 
-        if (cacheMode != CacheMode.REPLICATED)
+        if (cacheMode != REPLICATED)
             ccfg.setBackups(backups);
 
         if (near)
             ccfg.setNearConfiguration(new NearCacheConfiguration<>());
 
-        ccfg.setRebalanceMode(SYNC);
+        ccfg.setRebalanceMode(ASYNC);
 
         return ccfg;
     }
